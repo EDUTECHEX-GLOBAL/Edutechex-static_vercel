fd86ccda64bb9b27ef10fcdf683459aa850719f7
Anu1027 <anuradhasomisetti1027@gmail.com>
2026-04-02 16:45:27 +0530

    2april2026 ui changes

--- src/header.js
+++ src/header.js
@@ -15,19 +15,40 @@ const Header = () => {
           {/* Logo */}
           <img src={logo} alt="EDUTECH" className="logo-img" />
 
-          {/* Category Button - no icon, plain text only */}
+          {/* Category Button - with grid icon like Angular */}
           <button className="category-btn">
+            <i className="bi bi-ui-radios-grid category-icon"></i>
             <span>Category</span>
           </button>
         </div>
 
         {/* NAVIGATION */}
         <nav className={`nav ${isMobileMenuOpen ? 'active' : ''}`}>
-          <button className="nav-link">Courses ▾</button>
-          <button className="nav-link">Products ▾</button>
-          <button className="nav-link">Consulting ▾</button>
+          <button className="nav-link">
+            Courses
+            <svg className="chevron-icon" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <path fillRule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+            </svg>
+          </button>
+          <button className="nav-link">
+            Products
+            <svg className="chevron-icon" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <path fillRule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+            </svg>
+          </button>
+          <button className="nav-link">
+            Consulting
+            <svg className="chevron-icon" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <path fillRule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+            </svg>
+          </button>
           <button className="nav-link buy">Buy Courses</button>
-          <button className="nav-link">More ▾</button>
+          <button className="nav-link">
+            More
+            <svg className="chevron-icon" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <path fillRule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+            </svg>
+          </button>
         </nav>
 
         {/* RIGHT SECTION */}
